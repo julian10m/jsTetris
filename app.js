ec2a63ddf05b8d8506584af4364dd73a566ac02b
julian10m <35936197+julian10m@users.noreply.github.com>
2022-04-08 23:46:45 +0200

    simplified and merged right/left functions

--- app.js
+++ app.js
@@ -46,117 +46,159 @@ const getRandomCharacteristics = () => [
 const getTetromino = (shapeIdx, rotationIdx) =>
   tetrominoes[shapeIdx][rotationIdx];
 
-const freeze = function () {
-  if (
-    currentTetromino.some(
-      (index) =>
-        currentPosition + index + width >= squares.length ||
-        squares[currentPosition + index + width].classList.contains("taken")
-    )
-  ) {
-    console.log("Freezeing now");
-    currentTetromino.forEach((index) =>
-      squares[currentPosition + index].classList.add("taken")
-    );
-    if (currentTetromino.some((index) => currentPosition + index < width)) {
-      console.log("turning off timer");
-      isFinished = true;
-      clearInterval(timerId);
-      timerId = null;
-    } else {
-      [currentShape, currentRotation] = getRandomCharacteristics();
-      currentTetromino = getTetromino(currentShape, currentRotation);
-      currentPosition = 4 + getStartingPosition(...currentTetromino);
-      draw();
-    }
-  }
-};
+// const freeze = function () {
+//   if (
+//     currentTetromino.some(
+//       (index) =>
+//         currentPosition + index + width >= squares.length ||
+//         squares[currentPosition + index + width].classList.contains("taken")
+//     )
+//   ) {
+//     console.log("Freezeing now");
+//     currentTetromino.forEach((index) =>
+//       squares[currentPosition + index].classList.add("taken")
+//     );
+//     if (currentTetromino.some((index) => currentPosition + index < width)) {
+//       console.log("turning off timer");
+//       isFinished = true;
+//       clearInterval(timerId);
+//       timerId = null;
+//     } else {
+//       [currentShape, currentRotation] = getRandomCharacteristics();
+//       currentTetromino = getTetromino(currentShape, currentRotation);
+//       currentPosition = 4 + getStartingPosition(...currentTetromino);
+//       draw();
+//     }
+//   }
+// };
+
+const shouldAlreadyBeVisible = (index) => currentPosition + index >= 0;
 
 const draw = function () {
   currentTetromino.forEach((index) => {
-    if (currentPosition + index >= 0)
+    if (shouldAlreadyBeVisible(index))
       squares[currentPosition + index].classList.add("tetromino");
   });
 };
 
+const cannotBeDisplayedAtCurrentPosition = (tetromino) =>
+  tetromino.some(
+    (index) =>
+      shouldAlreadyBeVisible(index) &&
+      (currentPosition + index >= squares.length ||
+        squares[currentPosition + index].classList.contains("taken"))
+  );
+
+const loadAndRenderNewTetromino = function () {
+  [currentShape, currentRotation] = getRandomCharacteristics();
+  currentTetromino = getTetromino(currentShape, currentRotation);
+  currentPosition = 4 + getStartingPosition(...currentTetromino);
+  render(0);
+};
+
+const clearTimer = function () {
+  clearInterval(timerId);
+  timerId = null;
+};
+
+const markGrid = function (tetromino) {
+  tetromino.forEach((index) => {
+    if (shouldAlreadyBeVisible(index))
+      squares[currentPosition + index].classList.add("taken");
+  });
+};
+
+const render = function (step) {
+  if (!cannotBeDisplayedAtCurrentPosition(currentTetromino)) draw();
+  else {
+    currentPosition -= step;
+    draw();
+    markGrid(currentTetromino);
+    if (currentTetromino.some((index) => currentPosition + index < 0)) {
+      console.log("turning off timer");
+      isFinished = true;
+      clearTimer();
+    } else {
+      loadAndRenderNewTetromino();
+    }
+  }
+};
+
 const undraw = function () {
   currentTetromino.forEach((index) => {
-    if (currentPosition + index >= 0)
+    if (shouldAlreadyBeVisible(index))
       squares[currentPosition + index].classList.remove("tetromino");
   });
 };
 
 const move = function (step) {
-  freeze();
+  //   freeze();
   undraw();
   currentPosition += step;
-  draw();
+  render(step);
 };
 
 const moveDown = function () {
   move(width);
 };
 
-const isAtLeftEdge = function (tetromino) {
-  return tetromino.some((index) => (currentPosition + index) % width === 0);
-};
+const isAtEdge = (tetromino, edgeIdx) =>
+  tetromino.some((index) => (currentPosition + index) % width === edgeIdx);
+// const isAtLeftEdge = (tetromino) => isAtEdge(tetromino, 0);
+// const isAtRightEdge = (tetromino) => isAtEdge(tetromino, width - 1);
 
-const hasTetrominoAtLeft = function (tetromino) {
-  return tetromino.some((index) =>
-    squares[index + currentPosition - 1].classList.contains("taken")
+const hasTetrominoAtSide = (tetromino, deltaSide) =>
+  tetromino.some((index) =>
+    squares[index + currentPosition + deltaSide].classList.contains("taken")
   );
-};
+// const hasTetrominoAtLeft = (tetromino) => hasTetrominoAtSide(tetromino, -1);
+// const hasTetrominoAtRight = (tetromino) => hasTetrominoAtSide(tetromino, 1);
 
-const moveLeft = function () {
+const moveSide = function (edgeIdx, deltaSide) {
   if (currentPosition < 0) return;
   if (
-    !isAtLeftEdge(currentTetromino) &&
-    !hasTetrominoAtLeft(currentTetromino)
+    !isAtEdge(currentTetromino, edgeIdx) &&
+    !hasTetrominoAtSide(currentTetromino, deltaSide)
   ) {
-    move(-1);
+    move(deltaSide);
   }
 };
 
-const isAtRightEdge = function (tetromino) {
-  return tetromino.some(
-    (index) => (currentPosition + index) % width === width - 1
-  );
-};
+const moveLeft = () => moveSide(0, -1);
+const moveRight = () => moveSide(width - 1, 1);
 
-const hasTetrominoAtRight = function (tetromino) {
-  return tetromino.some((index) =>
-    squares[index + currentPosition + 1].classList.contains("taken")
-  );
-};
+// const moveLeft = function () {
+//   if (currentPosition < 0) return;
+//   if (
+//     !isAtLeftEdge(currentTetromino) &&
+//     !hasTetrominoAtLeft(currentTetromino)
+//   ) {
+//     move(-1);
+//   }
+// };
 
-const moveRight = function () {
-  if (currentPosition < 0) return;
-  if (
-    !isAtRightEdge(currentTetromino) &&
-    !hasTetrominoAtRight(currentTetromino)
-  ) {
-    move(1);
-  }
-};
-
-const isInvalidRotation = function (tetromino) {
-  return (
-    (isAtLeftEdge(tetromino) && isAtRightEdge(tetromino)) ||
-    (currentPosition >= 0 &&
-      tetromino.some(
-        (index) =>
-          currentPosition + index >= squares.length ||
-          squares[currentPosition + index].classList.contains("taken")
-      ))
-  );
-};
+// const moveRight = function () {
+//   if (currentPosition < 0) return;
+//   if (
+//     !isAtRightEdge(currentTetromino) &&
+//     !hasTetrominoAtRight(currentTetromino)
+//   ) {
+//     move(1);
+//   }
+// };
 
 const rotate = function () {
-  console.log("rotate");
+  //   console.log("rotate");
   let nextRotation = currentRotation + 1;
   if (nextRotation === tetrominoes[currentShape].length) nextRotation = 0;
   const nextTetromino = tetrominoes[currentShape][nextRotation];
-  if (!isInvalidRotation(nextTetromino)) {
+  if (
+    !(
+      (isAtEdge(nextTetromino, 0) && isAtEdge(nextTetromino, width - 1)) ||
+      //   (isAtLeftEdge(nextTetromino) && isAtRightEdge(nextTetromino)) ||
+      cannotBeDisplayedAtCurrentPosition(nextTetromino)
+    )
+  ) {
     undraw();
     currentRotation = nextRotation;
     currentTetromino = nextTetromino;
@@ -181,15 +223,12 @@ const width = 10;
 const tetrominoes = getTetrominoes();
 
 let isFinished = false;
-let [currentShape, currentRotation] = getRandomCharacteristics();
-let currentTetromino = getTetromino(currentShape, currentRotation);
-let currentPosition = 4 + getStartingPosition(...currentTetromino);
-let timerId = setInterval(moveDown, 500);
-document.addEventListener("keyup", applyTransformation);
+let currentShape, currentRotation, currentTetromino, currentPosition;
+loadAndRenderNewTetromino();
+let timerId = setInterval(moveDown, 1000);
+document.addEventListener("keydown", applyTransformation);
+
 startBtn.addEventListener("click", function () {
   if (!timerId) timerId = setInterval(moveDown, 500);
-  else {
-    clearInterval(timerId);
-    timerId = null;
-  }
+  else clearTimer();
 });
